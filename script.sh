5ef22e576686adc781a552e1b76b71ae78b5004e
asemsoufi <asemsoufi@gmail.com>
2022-06-02 21:52:24 +0100

    initial commit

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir fol_1 fol_2
+cd fol_1
+touch 1_1.txt 1_2.txt 1_3.txt
+cd ../
+cd fol_2
+touch 2_1.txt 2_2.txt 2_3.txt
+cd ../
+touch .gitignore
+echo fol_1/ > .gitignore
+echo fol_2/ >> .gitignore
